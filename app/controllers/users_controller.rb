25a2eb477de4e9fe8fce618181636f61111aa2e1
nadavschwartz <nadavschwartz58@gmail.com>
2020-09-10 08:13:47 -0700

    add basic user signup logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_data)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+    private
+  def user_data
+    params.require(:user).permit(:rname, :password, :email)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :password, length: { in: 6..72 }
+    validates_presence_of :email, :name
+    validates_uniqueness_of :email, { case_sensitive: false }
 end
